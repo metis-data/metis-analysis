70e0c3703c0f7687d49b4b8c577b60f10fba3fd4
gideonshmila <gideons@metisdata.io>
2023-03-28 14:00:52 +0300

    p

--- reports/index.js
+++ reports/index.js
@@ -1,9 +1,8 @@
-
 const { v4: uuidv4 } = require('uuid');
 
 const TAGS = new Set(['schema', 'table', 'index']);
 
- const processRows =  (databaseName, databaseHost, queryResult, timestamp) =>  {
+const processRows = (databaseName, databaseHost, queryResult, timestamp) => {
   const metricsData = [];
   queryResult.forEach((row) => {
     const valueNames = Object.keys(row).filter((key) => !TAGS.has(key));
@@ -13,22 +12,24 @@ const TAGS = new Set(['schema', 'table', 'index']);
       metric.timestamp = timestamp;
       metric.metricName = valueName;
       metric.value = parseFloat(row[valueName]);
-  
-      TAGS.forEach((tag) => { if (row[tag]) metric[tag] = row[tag]; });
+
+      TAGS.forEach((tag) => {
+        if (row[tag]) metric[tag] = row[tag];
+      });
       metric.db = databaseName;
       metric.host = databaseHost;
       metric.version = '0.63'; //hard coded and probably not true
       metricsData.push(metric);
     });
   });
-  
-  return metricsData;
-}
 
-  const processResults = (databaseName, databaseHost, results, timestamp) => {
-  const res = results.map((queryResult) => processRows(databaseName, databaseHost, queryResult, timestamp));
+  return metricsData;
+};
+
+const processResults = (databaseName, databaseHost, results, timestamp) => {
+  const res = processRows(databaseName, databaseHost, results, timestamp);
 
   return res;
-}
+};
 
 module.exports = { processResults };
